c03b0c49cdf233f91876d8f97cb12ff7fbe1e57d
M3LL00W <saicharankasarapu2004@gmail.com>
2025-09-16 03:28:47 +0530

    loading glitch of notes not found(resolved)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -37,12 +37,14 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
         <Navbar/>
-
-        {isRateLimited && <RatelimitedUi/>}
         <div className='max-w-7xl mx-auto p-4 mt-6'>
-            {loading && <div className='text-center text-primary py-10'>Loading Notes...</div> }
-            {notes.length===0 && !isRateLimited && <NotesNotFound/>}
-            {notes.length > 0 && !isRateLimited && (
+            {loading ? (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        ) : isRateLimited ? (
+          <RateLimitedUI />
+        ) : notes.length === 0 ? (
+          <NotesNotFound />
+        ) : (
                 <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {notes.map(note=>(
                         <NoteCard key={note._id} note={note} setNotes={setNotes}/>
